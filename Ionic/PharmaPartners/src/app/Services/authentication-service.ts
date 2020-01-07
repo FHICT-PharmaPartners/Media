99361ee974892944324a2c43fbb7c492156ca66f
Isaidhello <a.bye@student.fontys.nl>
2020-01-07 10:12:32 +0100

    frontend improvement
    hi

--- Ionic/PharmaPartners/src/app/Services/authentication-service.ts
+++ Ionic/PharmaPartners/src/app/Services/authentication-service.ts
@@ -12,26 +12,5 @@ export class AuthenticationService {
     private user = Object;
 
 
-    authenticate(username, password) {
 
-        const opts = {
-            headers: new HttpHeaders({
-                'Content-Type': 'application/json'
-            })
-        };
-
-        const jwt = localStorage.getItem(`Token`);
-
-        if (!jwt) {
-            location.href = `/login`;
-        } else {
-            return new Promise((resolve) => {
-                this.http.post(this.apiURL + `/` + jwt, JSON.stringify(username, password), opts)
-                    .subscribe((response) => {
-                        this.user = response;
-                        resolve(this.user);
-                    });
-            });
-        }
-    }
 }
