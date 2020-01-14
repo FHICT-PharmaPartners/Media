7ec33d2c5fd6d9a4d5e56782ad1a59b0b3607892
Martijn Verwijmeren <47419448+v-martijn@users.noreply.github.com>
2020-01-14 13:41:32 +0100

    Update login.page.ts

--- Ionic/PharmaPartners/src/app/login/login.page.ts
+++ Ionic/PharmaPartners/src/app/login/login.page.ts
@@ -12,19 +12,19 @@ export class LoginPage implements OnInit {
     constructor(private dataService: LoginService) {
     }
 
-    // login() {
-    //     const username = this.username;
-    //     const password = this.password;
-    //
-    //     return this.dataService.login(username, password)
-    //         .then(data => {
-    //             const jwt = data.jwt;
-    //             localStorage.setItem(`Token`, jwt);
-    //             location.href = `/home`;
-    //         }).catch(err => {
-    //             alert(err);
-    //         });
-    // }
+    login() {
+        const username = this.username;
+        const password = this.password;
+    
+        return this.dataService.login(username, password)
+            .then(data => {
+                const jwt = data.jwt;
+                localStorage.setItem(`Token`, jwt);
+                location.href = `/home`;
+            }).catch(err => {
+                alert(err);
+            });
+    }
 
     ngOnInit() {
     }
